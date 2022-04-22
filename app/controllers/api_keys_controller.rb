a4a9d0142e9fefd9f11106143cad1efc86f2af7d
Albert Pangilinan <rapangil@uwaterloo.ca>
2022-04-22 16:50:10 -0400

    fixed api auth

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -4,6 +4,7 @@ class ApiKeysController < ApplicationController
   protect_from_forgery with: :null_session, prepend: true
 
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
    
   # Require token authentication for index                             
   prepend_before_action :authenticate_with_api_key!, only: [:index] 
@@ -12,21 +13,19 @@ class ApiKeysController < ApplicationController
   prepend_before_action :authenticate_with_api_key, only: [:destroy]
 
   def index
-    render json: current_bearer.api_keys
+    render(json: current_bearer.api_keys)
   end
 
   def create
     authenticate_with_http_basic do |email, password| 
-      user = User.find_by email: email 
- 
-      if user
+      user = User.find_for_authentication(email: email)
+
+      if user.valid_password?(password)
         api_key = user.api_keys.create! token: SecureRandom.hex 
- 
-        render json: api_key, status: :created and return 
+        render(json: api_key, status: :created) and return 
       end 
     end
- 
-    render status: :unauthorized
+    render(status: :unauthorized)
   end
 
   def destroy
